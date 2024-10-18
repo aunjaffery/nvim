46c8959f0130fc3f2202eb6048d2cbe457daafa9
aun <aunjaffery123@gmail.com>
2024-10-18 18:53:43 +0500

    blankline

--- lua/aunox/plugins/blankline.lua
+++ lua/aunox/plugins/blankline.lua
@@ -1,9 +1,9 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" },
-    scope = {enabled = false}
-  },
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	opts = {
+		indent = { char = "┊" },
+		scope = { enabled = false },
+	},
 }
